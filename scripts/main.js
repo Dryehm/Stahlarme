124cc17f5f7765e0c648d97cce46dc6d3431c69e
Dryehm <79123285+Dryehm@users.noreply.github.com>
2023-07-08 11:17:17 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 Events.on(ClientLoadEvent, b  => {
-Vars.content.getByName(ContentType.block, "stahlarme-305mmL50").ammoTypes.get(Items.copper).splashDamageRadius = 25
-Vars.content.getByName(ContentType.block, "stahlarme-305mmL50").ammoTypes.get(Items.copper).splashDamage = 1000
-Vars.content.getByName(ContentType.block, "stahlarme-305mmL50").ammoTypes.get(Items.lead).splashDamageRadius = 75
-Vars.content.getByName(ContentType.block, "stahlarme-305mmL50").ammoTypes.get(Items.lead).splashDamage = 500
+Vars.content.getByName(ContentType.block, "stahlarme-305mmL50").ammoTypes.get(Items.copper).splashDamageRadius = 50
+Vars.content.getByName(ContentType.block, "stahlarme-305mmL50").ammoTypes.get(Items.copper).splashDamage = 1500
+Vars.content.getByName(ContentType.block, "stahlarme-305mmL50").ammoTypes.get(Items.lead).splashDamageRadius = 250
+Vars.content.getByName(ContentType.block, "stahlarme-305mmL50").ammoTypes.get(Items.lead).splashDamage = 700
 });
